9368dc351ab72ce8b006a27b99d52285ac54e218
Arshad Aiyoob <arshadaiyoob19@gmail.com>
2019-03-01 22:51:49 +0530

    Update Firebase Remote Config and added Team Fragment

---
+++ app/src/main/java/com/izar/dev/utce/Adapters/TeamAdapter.java
@@ -0,0 +1,4 @@
+package com.izar.dev.utce.Adapters;
+
+public class TeamAdapter {
+}
--- app/src/main/java/com/izar/dev/utce/RecyclerAdapter/TeamData.java
+++ app/src/main/java/com/izar/dev/utce/RecyclerAdapter/TeamData.java
@@ -1,4 +1,40 @@
 package com.izar.dev.utce.RecyclerAdapter;
 
 public class TeamData {
+    private String name;
+    private String thumbnail;
+    private String numOfScore;
+
+    public TeamData(){
+
+    }
+    public TeamData(String name, String thumbnail,String numOfScore){
+        this.name =name;
+        this.thumbnail = thumbnail;
+        this.numOfScore = numOfScore;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getthumbnail() {
+        return thumbnail;
+    }
+
+    public void setthumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getnumOfScore() {
+        return numOfScore;
+    }
+
+    public void setnumOfScore(String numOfScore) {
+        this.numOfScore = numOfScore;
+    }
 }
